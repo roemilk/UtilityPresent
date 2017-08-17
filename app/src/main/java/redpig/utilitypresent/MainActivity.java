5a95ba81cbae3ec9c50856fbc810461421d84692
roe <roemilk@gmail.com>
2017-08-17 14:49:39 +0900

    1. 샘플 코드 수정

--- app/src/main/java/redpig/utilitypresent/MainActivity.java
+++ app/src/main/java/redpig/utilitypresent/MainActivity.java
@@ -1,81 +1,41 @@
 package redpig.utilitypresent;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import redpig.utility.gps.GpsInfo;
+import redpig.utility.gps.GpsInterface;
+import redpig.utility.gps.GpsManager;
+import redpig.utility.gps.GpsUtils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GpsInterface.onUpdateListener {
     private final String TAG = "TEST";
 
-    private EditText mDbNameEditText, mTbEditText;
-    private Button mCreateDbButton, mCreateTbButton;
-    private TextView mResultTextView;
-
-    SQLiteDatabase db;
+    private Button mGpsButton, mGpsStopButton;
+    private TextView mGpsTextView;
+    private GpsManager mGPGpsManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDbNameEditText = (EditText)findViewById(R.id.editText);
-        mTbEditText = (EditText)findViewById(R.id.editText2);
-        mCreateDbButton = (Button)findViewById(R.id.button);
-        mCreateTbButton = (Button)findViewById(R.id.button2);
-        mResultTextView = (TextView)findViewById(R.id.result_textView);
+        mGpsButton = (Button)findViewById(R.id.button3);
+        mGpsStopButton = (Button)findViewById(R.id.button);
+        mGpsTextView = (TextView)findViewById(R.id.textView);
+        mGpsButton.setOnClickListener(this);
+        mGpsStopButton.setOnClickListener(this);
 
-        mCreateDbButton.setOnClickListener(this);
-        mCreateTbButton.setOnClickListener(this);
-
-//        Fragment fragment = new SettingsScreen();
-//        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-//        if(savedInstanceState == null){
-//            // crated for the first time
-//            fragmentTransaction.add(R.id.container, fragment, "settings_fragment");
-//            fragmentTransaction.commit();
-//        }else{
-//            fragment = getFragmentManager().findFragmentByTag("settings_fragment");
-//
-//        }
-
-
-
-    }
-
-    private void createDatabase(String name){
-
-    }
-
-    private void createTable(){
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        switch (v.getId()){
-            case R.id.button :
-                String dbName = mDbNameEditText.getText().toString().trim();
-
-                break;
-            case R.id.button2 :
-
-                break;
-        }
-
-    }
-
-
-    public static class SettingsScreen extends PreferenceFragment{
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.setting_screen);
+        boolean gpsCheck = GpsUtils.getGpsState(this);
+        if(gpsCheck){
+            Toast.makeText(this, "GPS 사용이 가능합니다.", Toast.LENGTH_SHORT).show();
+            mGPGpsManager = GpsManager.getGpsManager(this, this);
+        }else{
+            Toast.makeText(this, "GPS 사용이 불가능합니다.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -98,7 +58,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onBackPressed() {
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button3 :
+                mGPGpsManager.startGpsService(100, 0, true);
+                break;
+            case R.id.button :
+                mGPGpsManager.stopGpsService();
+                break;
+        }
+    }
+
+    @Override
+    public void updateGpsInfo(GpsInfo gpsinfo) {
+        double latitude = gpsinfo.getLatitude();
+        double longitue = gpsinfo.getLongitude();
+        float speed = gpsinfo.getSpeed();
+        float bearing = gpsinfo.getBearing();
+        mGpsTextView.setText("위도 : " + latitude + " 경도 : " + longitue + " 속도 : " + speed + " 방향 : " + bearing);
     }
 }
 
